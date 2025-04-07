367542497a5586edaa83fd58a92c4012d482e267
Hoang Phuong <phuonggm204@gmail.com>
2025-04-07 14:46:20 +0700

    P

--- BookingCare263/app/src/main/java/com/example/bookingcare263/ui/uiuser/HomeFragment.java
+++ BookingCare263/app/src/main/java/com/example/bookingcare263/ui/uiuser/HomeFragment.java
@@ -174,12 +174,11 @@ public class HomeFragment extends Fragment {
                 // get listbac bysdt
                 listDoctors.clear();
                 for (accout accout : listaccout) {
-                    FirebaseHelper.getttBacsiBySdt(accout.getPhone(), new FirebaseCallBack<Bacsi>(){
+                    FirebaseHelper.getBacsiBySdt(accout.getPhone(), new FirebaseCallBack<Bacsi>(){
                         @Override
                         public void onSuccess(Bacsi data) {
                             listDoctors.add(data);
 
-                            if(listDoctors.size() == listaccout.size())
                                 adapterDoctors.notifyDataSetChanged();
 
                     }
