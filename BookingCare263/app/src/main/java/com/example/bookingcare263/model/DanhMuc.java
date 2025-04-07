ae13e28e11f450a215c641074d04b5e4e91c4f6e
DoanAnhHau <anhhau873@gmail.com>
2025-04-07 14:43:12 +0700

    H

---
+++ BookingCare263/app/src/main/java/com/example/bookingcare263/model/DanhMuc.java
@@ -0,0 +1,4 @@
+package com.example.bookingcare263.model;
+
+public class DanhMuc {
+}
--- BookingCare263/app/src/main/java/com/example/bookingcare263/ui/uiuser/KhamTongQuaActivity.java
+++ BookingCare263/app/src/main/java/com/example/bookingcare263/ui/uiuser/KhamTongQuaActivity.java
@@ -12,10 +12,7 @@ import com.example.bookingcare263.R;
 
 public class KhamTongQuaActivity extends AppCompatActivity {
 
-    private EditText thanhTimKiem;
-    private Button nutKhuVuc, nutDanhMuc, nutGia, nutCoSoYTe;
-    private HorizontalScrollView thanhCuonDanhMuc, thanhCuonGoiNoiBat, thanhCuonCoSoYTe;
-    private Spinner spinRole; // spinner mà bạn muốn sử dụng
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,30 +20,6 @@ public class KhamTongQuaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_kham_tong_qua); // Giả sử layout của bạn là activity_kham_tong_quat.xml
 
         // Khởi tạo các view
-        thanhTimKiem = findViewById(R.id.thanh_tim_kiem);
-        nutKhuVuc = findViewById(R.id.nut_khu_vuc);
-        nutDanhMuc = findViewById(R.id.nut_danh_muc);
-        nutGia = findViewById(R.id.nut_gia);
-        nutCoSoYTe = findViewById(R.id.nut_co_so_y_te);
-        thanhCuonDanhMuc = findViewById(R.id.thanh_cuon_danh_muc);
-        thanhCuonGoiNoiBat = findViewById(R.id.thanh_cuon_goi_noi_bat);
-        thanhCuonCoSoYTe = findViewById(R.id.thanh_cuon_co_so_y_te);
 
-        // Xử lý sự kiện nút
-        nutKhuVuc.setOnClickListener(view -> {
-            // Mở hoặc hiển thị menu chọn khu vực
-        });
-
-        nutDanhMuc.setOnClickListener(view -> {
-            // Mở hoặc hiển thị menu chọn danh mục
-        });
-
-        nutGia.setOnClickListener(view -> {
-            // Mở hoặc hiển thị menu chọn mức giá
-        });
-
-        nutCoSoYTe.setOnClickListener(view -> {
-            // Mở hoặc hiển thị menu chọn cơ sở y tế
-        });
     }
 }
