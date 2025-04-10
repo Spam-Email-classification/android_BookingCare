f9ae1077e85f375d60f1288fb8a74939ff6d5dbf
Hoang Phuong <phuonggm204@gmail.com>
2025-04-10 21:32:31 +0700

    Phuong

---
+++ BookingCare263/app/src/main/java/com/example/bookingcare263/ui/bacsiui/ThongtinBenhnhan.java
@@ -0,0 +1,26 @@
+package com.example.bookingcare263.ui.bacsiui;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.bookingcare263.R;
+
+public class ThongtinBenhnhan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_thongtin_benhnhan);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
